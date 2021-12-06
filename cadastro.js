dc973c1b5789ca8482eccd01d74541d0e35ad8d9
fermuniz83 <fercomobone@hotmail.com>
2021-12-06 20:15:54 -0300

    cadastro

---
+++ cadastro.js
@@ -0,0 +1,22 @@
+const dataDoEvento = "25/12/2021"
+if (dataDoEvento<= "25/12/2021"){
+    console.log ("cadastro efetuado com sucesso ")
+}
+else{
+    console.log("evento não está disponivel")
+}
+
+const limiteDeIdade=18
+if(limiteDeIdade>=18){
+    console.log ("cadastro efetuado com sucesso")
+}
+else{
+    console.log("evento proibido para menores de 18 anos ")
+}
+const limiteDeCapacidade = 100
+if (limiteDeCapacidade<=100){
+    console.log("cadastro efetuado com sucesso")
+}
+else{
+    console.log("o evento já está lotado")}
+    
